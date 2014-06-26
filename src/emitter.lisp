49f39f5ae528e1bbd26ef989d80b940362e172da
Fernando Borretti <eudoxiahp@gmail.com>
2014-06-26 20:54:00 -0300

    Structure some things

--- src/emitter.lisp
+++ src/emitter.lisp
@@ -1,10 +1,29 @@
 (in-package :cl-user)
 (defpackage wax.emitter
   (:use :cl)
-  (:export :emit))
+  (:export :+default-output-type+
+           :emit))
 (in-package :wax.emitter)
 
+(defparameter *output-types* (list))
+
+(defmacro defbackend (name)
+  `(push-new ,name *output-types*))
+
+(defparameter +output-types+ (list :html :tex))
 (defparameter +default-output-type+ :html)
+
+(defparameter *contexts* (list))
+
+(defmacro defcontext (name)
+  `(push ,(string-upcase (symbol-name name)) *contexts*))
+
+(defcontext global)
+
+(defmacro deftag (outout-type name (&optional (context global))
+                  (&rest args) &rest body)
+  (
+
 
 (defun emit (tree output-type)
   (cond
