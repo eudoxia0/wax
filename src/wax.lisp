2767705a1c5f427b2ba1d5348cfb92ed33ea1af3
Fernando Borretti <eudoxiahp@gmail.com>
2014-06-27 22:38:41 -0300

    Tie the frontend into the emit function

--- src/wax.lisp
+++ src/wax.lisp
@@ -1,13 +1,17 @@
 (in-package :cl-user)
 (defpackage wax
   (:use :cl)
+  (:import-from :wax.parser
+                :parse-string)
+  (:import-from :wax.emitter
+                :emit)
   (:export :process))
 (in-package :wax)
 
-(defmethod process ((input string)
-                    &optional (output-type +default-output-type+))
-  (parse-string input))
+(defparameter +default-backend+ :html)
 
-(defmethod process ((input pathname)
-                    &optional (output-type +default-output-type+))
-  (process (uiop:read-file-string input output-type)))
+(defmethod process ((input string) &optional (backend +default-backend+))
+  (emit (parse-string input) backend))
+
+(defmethod process ((input pathname) &optional (backend +default-backend+))
+  (process (uiop:read-file-string input backende)))
