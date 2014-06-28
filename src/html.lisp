2f3af6936b7a1b27a87e50075d02012c6a1837ee
Fernando Borretti <eudoxiahp@gmail.com>
2014-06-27 22:38:29 -0300

    Properly add bold tag

--- src/html.lisp
+++ src/html.lisp
@@ -3,6 +3,9 @@
   (:use :cl :anaphora :wax.emitter :wax.utils))
 (in-package :wax.html)
 
+(defun print-tag (tag tree)
+  (format nil "<~A>~A</~A>" tag (print-tree tree) tag))
+
 (defbackend :html
   ;;; Basic formatting
-  (defrule b () (&rest text) (print-tree text)))
+  (defrule b () (&rest text) (print-tag "strong" text)))
