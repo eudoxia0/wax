75ee2d384a47834aecab53efe1627430cbebf194
Fernando Borretti <eudoxiahp@gmail.com>
2014-10-05 22:24:52 -0200

    Update the Emacs mode

--- emacs/wax-mode.el
+++ emacs/wax-mode.el
@@ -1,13 +1,6 @@
-(setq wax-functions-regexp
-      "([a-z]+")
-
-(setq wax-font-lock-keywords
-  `((,wax-functions-regexp . font-lock-function-name-face)))
-
-(define-derived-mode wax-mode lisp-mode "Wax markup language"
+(define-derived-mode wax-mode tex-mode "Wax markup language"
   "Wax mode"
-  (setq font-lock-defaults '((wax-font-lock-keywords)))
-  (setq comment-start ";")
+  (setq comment-start ";;")
   (setq comment-end ""))
 
 (provide 'wax-mode)
